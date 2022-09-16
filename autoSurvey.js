89fb5f389d5aba42c6c0d7de28e40ef0c7f619a0
dathdka <dathdka@gmail.com>
2022-09-16 10:42:40 +0700

    extension running after DOM completed loading

--- autoSurvey.js
+++ autoSurvey.js
@@ -1,49 +1,50 @@
-//check all
-// var checkList = document.getElementsByClassName('ng-untouched ng-pristine ng-valid');
-// const check = (list) =>{
-//     for (let index = 0; index < list.length; index++) {
-//         list[index].checked = true;
-//     }
-// }
-
 //custom check
-const autoComplete = () =>{
-    var choice =  prompt('L: low\n    M: Medium\n     H: High');
-    var min, max;
-    switch (choice) {
-        case 'L', 'l':
-            min = 1;
-            max = 3;
-            break;
-        case 'M', 'm':
-            min = 3;
-            max = 5;
-            break;
-        case 'H', 'h':
-            min = 4;
-            max = 6;
-            break;
-        default:
-            min = 1;
-            max = 6;
-            break;
-    }
-    var query = stringBuilder(1192, min , max - 1);
-    var target = document.getElementById(query);
-    if(target)
-        target.checked = true;
-    for(let i = 1194 ; i<= 1219; i++){
-        query = stringBuilder(i,min,max);
-        target = document.getElementById(query);
-        if(target){
-            target.checked = true;
-        }
-    }
-}
- 
-const stringBuilder = (number,min,max) =>{
-    const answer =  Math.floor(Math.random()* (max - min) + min);
-    return `cauhoi${number}dapan${answer}`
-}
-autoComplete()
+const autoComplete = () => {
+  var choice = prompt("     L: low\n     M: Medium\n     H: High");
+  var min, max;
+  switch (choice) {
+    case ("L", "l"):
+      min = 1;
+      max = 3;
+      break;
+    case ("M", "m"):
+      min = 3;
+      max = 5;
+      break;
+    case ("H", "h"):
+      min = 4;
+      max = 6;
+      break;
+    default:
+      min = 1;
+      max = 6;
+      break;
+  }
+  for (let i = 1; i <= 2000; i++) {
+    query = stringBuilder(i, min, max);
+    target = document.getElementById(query);
+    if (target) target.click();
+    else if ((target = document.getElementById(stringBuilder(i, min, max - 1))))
+      target.click();
+  }
+};
 
+const stringBuilder = (number, min, max) => {
+  const answer = Math.floor(Math.random() * (max - min) + min);
+  return `cauhoi${number}dapan${answer}`;
+};
+
+window.addEventListener("load", waitForLoad, false);
+function waitForLoad(evt) {
+  var jsInitChecktimer = setInterval(checkForJS_Finish, 111);
+
+  function checkForJS_Finish() {
+    if (
+      document.querySelector("#cauhoi1192dapan1") ||
+      document.querySelector("#cauhoi788dapan1")
+    ) {
+      clearInterval(jsInitChecktimer);
+      autoComplete();
+    }
+  }
+}
